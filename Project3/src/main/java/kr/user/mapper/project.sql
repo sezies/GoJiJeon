d830bad2ab0b146cc6e76a713aa684102f51a062
smhrd <smhrd@DESKTOP-IN9VDQ2>
2021-07-29 10:47:13 +0900

    2021/07/29 기초땅작업

---
+++ Project3/src/main/java/kr/user/mapper/project.sql
@@ -0,0 +1,14 @@
+create table (
+	num int not null auto_increment,
+	title varchar(50) not null,
+	author varchar(30) not null,
+	company varchar(50) not null,
+	isbn varchar(30) not null,
+	count int default 0,
+	primary key(num)
+
+);
+
+
+select * from 
+
