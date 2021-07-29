cf411f0f76149ad5a0cce846bbdb2054399b5310
smhrd <smhrd@DESKTOP-IN9VDQ2>
2021-07-29 12:21:28 +0900

    -2021 07 29 VO,SQL 추가

--- Project3/src/main/java/kr/user/mapper/project.sql
+++ Project3/src/main/java/kr/user/mapper/project.sql
@@ -1,14 +1,22 @@
-create table (
-	num int not null auto_increment,
-	title varchar(50) not null,
-	author varchar(30) not null,
-	company varchar(50) not null,
-	isbn varchar(30) not null,
-	count int default 0,
-	primary key(num)
-
+create table users(
+        user_num int not null auto_increment,
+        user_id varchar(30) not null,
+        user_pw varchar(30) not null,
+        user_name varchar(15) not null,
+        user_bank varchar(30) not null,
+        primary key(user_num)
 );
 
-
-select * from 
-
+create table notice(
+        user_num int,
+        notice_num int not null auto_increment,
+        notice_code int not null,
+        notice_title varchar(100) not null,
+        pay_day varchar(50) not null,
+        pay_money int not null,
+        pay_bank varchar(30) not null,
+        img varchar(200) not null,
+        regist_day datetime default now(),
+        primary key(notice_num),
+        foreign key(user_num) references users(user_num)
+);
